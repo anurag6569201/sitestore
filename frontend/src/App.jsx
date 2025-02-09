14039f4a5d33cd0caf3163a029901255a8f7540f
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2025-02-09 15:30:00 +0530

    feat:navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+
+import Navbar from './pages/global/navbar/navbar'
+import HomeSlider from './pages/global/slider/HomeSlider'
 
 function App() {
 
   return (
     <>
-      <h1>Hii</h1>
+      <Navbar/>
+      <HomeSlider/>
     </>
   )
 }
